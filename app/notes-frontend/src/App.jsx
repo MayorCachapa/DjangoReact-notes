59407ff1d171b31607c9c36d1aafd9b6d55daa6a
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-07-14 16:36:35 +0200

    Initial setup of Frontend done
    Added features:
    
    Created React.js using Vite instead of vanilla download
    Installed (and tested) Tailwindcss
    Removed all pre-built components by Vite
    Created empty 'Header' and 'NotesListPage' to render all notes

---
+++ app/notes-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Header } from './components/header'
+import { NotesListPage } from './pages/NotesListPage'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <NotesListPage />
+    </div>
+  )
+}
+
+export default App
---
+++ app/notes-frontend/src/pages/NotesListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotesListPage = () => {
+  return (
+    <div>
+      AllNotes      
+    </div>
+  )
+}
