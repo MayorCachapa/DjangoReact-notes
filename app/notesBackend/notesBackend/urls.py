e47c6c10e90f7856da41e91be1e83d446cb895a1
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-07-17 14:47:14 +0200

    Final touches and additions

--- app/notesBackend/notesBackend/urls.py
+++ app/notesBackend/notesBackend/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from notes import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('notes.urls'))
+    path('api/', include('notes.urls')),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
